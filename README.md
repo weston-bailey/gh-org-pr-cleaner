b6db41e1fb26d1eb1c6cf22c087f1581503391aa
weston-bailey <wdbailey87@gmail.com>
2023-02-15 22:47:46 -0800

    updates md, debugs bot

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
-# Script to clear out all of the prs on an org
+# Pr Closer
+
+Tool to automate the process of closing all of the open pull requests on a gh organization
 
 ## Getting Started
 
 * run `python3 -m venv venv` to create a virtual enviroment
 * run  `. ./venv/bin/activate` to enter the virutal enviroment
 * run `pip3 -r requirements.txt` to install the required packages
-* `touch .env` TODO: add gh token to .env
-* run `python3 main.py` to run the script
+* `touch .env`, add a [classic github token](https://github.com/settings/tokens/new) with all auth scopes to the `.env` file like so:
+```sh
+GH_TOKEN=< your github token >
+```
+* run `python3 main.py < org name >` to run the script and close all open prs
+
+example usage: 
+```sh
+$ python3 main.py WDI-SEA
+
+Closing pull requests on command-line-murder-mystery, repo 1 of 463
+Closing pull requests on google-shopping-conditionals-loops, repo 2 of 463
+Closing pull requests on google-shopping-functions, repo 3 of 463
+Closing pull requests on tic-tac-toe, repo 4 of 463
+Closing pull requests on iterators-reddit, repo 5 of 463
+Closing pull requests on jquery-todo-list, repo 6 of 463
+	Closing pulls for https://api.github.com/repos/WDI-SEA/jquery-todo-list/pulls/175
+Closing pull requests on ajax-reddit-slideshow, repo 7 of 463
+	Closing pulls for https://api.github.com/repos/WDI-SEA/ajax-reddit-slideshow/pulls/208
+Closing pull requests on bootstrap-mockups, repo 8 of 463
+	Closing pulls for https://api.github.com/repos/WDI-SEA/bootstrap-mockups/pulls/71
+	Closing pulls for https://api.github.com/repos/WDI-SEA/bootstrap-mockups/pulls/70
+	Closing pulls for https://api.github.com/repos/WDI-SEA/bootstrap-mockups/pulls/69
+...
+```
---
+++ lib/PullCloser.py
@@ -0,0 +1,58 @@
+import requests
+import json
+
+class PullCloser:
+    """
+    tool to automate the closure of all pull requests in a github organizaiton
+    PullCloser(token: str = gh user token, org: str = gh organization name)
+    instance.run() -> closes all prs on all repos in the supplied org
+    """
+
+    def __init__(self, token: str, org: str):
+        """args: token (gh user token), org (gh organization)"""
+        self.__token = token
+        self.__org = org
+        self.__repos = []
+        self.__data = json.dumps({  "state": "closed" })
+        self.__headers = {
+            "Authorization": f"token {self.__token}",
+            "Accept": "application/vnd.github+json"
+        }
+
+    def __get_repos(self):
+        """builds a list of all repos in the org"""
+        get_url = lambda page : f"https://api.github.com/orgs/WDI-SEA/repos?per_page=100&page={page}"
+
+        page = 1
+        while True:
+            response = requests.get(get_url(page), headers=self.__headers)
+            print(f"fetching page {page} of repos")
+            response_json = response.json()
+            if len(response_json) == 0:
+                break
+
+            self.__repos += response_json
+            print(f"{len(self.__repos)} retrived so far")
+            page += 1
+
+        return self
+
+    def __close_pulls(self):
+        """iterate all repos and close all open prs"""
+
+        for i, repo in enumerate(self.__repos):
+            print(f"Closing pull requests on {repo['name']}, repo {i + 1} of {len(self.__repos)}") 
+            response = requests.get(f"https://api.github.com/repos/WDI-SEA/{repo['name']}/pulls?per_page=100", headers=self.__headers)
+            pulls = response.json()
+            for pull in pulls:
+                print(f"\tClosing pulls for {pull['url']}")
+                pull_response = requests.patch(pull['url'], headers=self.__headers, data=self.__data)
+
+        return self
+
+    def run(self):
+        """fetchs all repos in the supplied org, iterates them, and closes all open prs"""
+        self.__get_repos().__close_pulls()
+
+        return self
+
